a8623de42bfc099c79837863c737cdd564740797
JARS3N <JARS3N@users.noreply.github.com>
2023-05-23 12:50:28 -0400

    Update server.R

--- R/server.R
+++ R/server.R
@@ -11,38 +11,39 @@ server_app <- function() {
   library(fs)
   library(readxl)
   library(plates)
-  
+
   shinyServer(function(input, output) {
     observeEvent(input$dirsel, {
       E <- new.env()
       E$dir <- choose.dir()
       E$data <- barval::grab_validation_data(E$dir)
-      
-      
+
+
       E$median_target <- median(E$data$pH_target)
       E$Lot <- unique(E$data$Lot)
       ## KSV
       # message("ksv data")
       E$ksv <- filter(E$data,!is.na(KSV)) %>%
-        group_by(O2_target) %>% 
+        group_by(O2_target) %>%
         mutate(modz=barval::modified_z_score(KSV),
                cut = barval::modz_cuts(KSV),
                use = modz<=cut
-               ) %>% 
-        ungroup() %>% 
-        filter(use) %>% 
+               ) %>%
+        ungroup() %>%
+        filter(use) %>%
         summarize(
           .,
           AVG_KSV = mean(KSV),
           AVG_F0 = mean(F0),
           Median_ksv = median(KSV),
-          Median_F0 = median(F0)
+          Median_F0 = median(F0),
+          O2_target = median(O2_target)
         )
       ### GAIN
       # message("profile gain data")
       gain_lm <- lm(Gain ~ pH_target, data = E$data)
-      
-      
+
+
       E$coeffs <- tibble(
         target = median(unique(E$data$pH_target)),
         slope = round(coef(gain_lm)[2], 6),
@@ -50,8 +51,8 @@ server_app <- function() {
         Gain = (target * slope) + intercept,
         rsquared = round(summary(gain_lm)$r.squared, 6)
       )
-      
-      
+
+
       E$plot$pH_lm <-
         filter(E$data, is.na(KSV)) %>%
         ggplot(., aes(pH_target, Gain)) +
@@ -59,7 +60,7 @@ server_app <- function() {
         geom_smooth(method = "lm") +
         theme_bw() +
         ggtitle(
-          label = unique(E$data$raw$Lot),
+          label = unique(E$data$Lot),
           subtitle = paste0(
             "Rsquared: ",
             E$coeffs$rsquared,
@@ -70,8 +71,8 @@ server_app <- function() {
             E$coeffs$intercept
           )
         )
-      
-      
+
+
       ##### LED & Gain table
       message("set tables")
       E$gain_led_table <-
@@ -90,13 +91,13 @@ server_app <- function() {
           Gain_plus3SD = Gain_AVG + (3 *
                                        Gain_SD)
         )
-      
+
       ### Save output
       message("save outputs")
       writexl::write_xlsx(
-        x = list(E$data,
-                 E$gain_led_table,
-                 E$coeffs,
+        x = list(data=E$data,
+                 Gain_table=E$gain_led_table,
+                 pH_coefficients = E$coeffs,
                  E$ksv),
         path = file.path(file.path(
           E$dir,
