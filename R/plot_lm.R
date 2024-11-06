b9da4e4ac73e31a4d5e330ed3bd63d64226af3d3
JARS3N <JARS3N@users.noreply.github.com>
2024-11-06 12:29:07 -0500

    Update plot_lm.R

--- R/plot_lm.R
+++ R/plot_lm.R
@@ -1,21 +1,32 @@
-plot_lm<-function(data,coefs){
-#takes E$data as argument
-tryCatch({
-data %>%
-  ggplot(aes(x = as.factor(pH_target), y = Gain)) +  # Treat pH_target as discrete
-  geom_violin(fill = "lightblue", alpha = 0.3) +  # Violin plot for distribution
-  geom_point(position = position_jitter(width = 0.05), alpha = 0.33) +  # Overlayed points with jitter
-  geom_smooth(aes(group = 1), method = "lm", se = FALSE, color = "blue") +  # Overall linear fit
-  theme_minimal() +
-  labs(x = "pH Target", y = "Gain") +
-  ggtitle(
-    label = unique(data$Lot),
-    subtitle = paste0(
-      "Rsquared: ", coefs$rsquared, "\n",
-      "eq: Gain = (Target * ", coefs$slope, ") + ",
-      coefs$intercept
-    )
-  )
-              )
-          }, error = function(e) NULL)
+
+plot_lm <- function(data, coefs) {
+  #takes E$data as argument
+  tryCatch({
+    data %>%
+      ggplot(aes(x = as.factor(pH_target), y = Gain)) +  # Treat pH_target as discrete
+      geom_violin(fill = "lightblue", alpha = 0.3) +  # Violin plot for distribution
+      geom_point(position = position_jitter(width = 0.05), alpha = 0.33) +  # Overlayed points with jitter
+      geom_smooth(
+        aes(group = 1),
+        method = "lm",
+        se = FALSE,
+        color = "blue"
+      ) +  # Overall linear fit
+      theme_minimal() +
+      labs(x = "pH Target", y = "Gain") +
+      ggtitle(
+        label = unique(data$Lot),
+        subtitle = paste0(
+          "Rsquared: ",
+          coefs$rsquared,
+          "\n",
+          "eq: Gain = (Target * ",
+          coefs$slope,
+          ") + ",
+          coefs$intercept
+        )
+      )
+    
+  }, error = function(e)
+    NULL)
 }
