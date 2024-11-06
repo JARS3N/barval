5dfd457b1c4a04e4e2f3f0d3033e648712b85f43
JARS3N <JARS3N@users.noreply.github.com>
2024-11-06 10:30:02 -0500

    Create grab_coefs.R

---
+++ R/grab_coefs.R
@@ -0,0 +1,14 @@
+grab_coefs<-function(model,target){
+# model=gain_lm
+# target=E$gain$pH_target
+  tryCatch({
+            tibble(
+              target = median(unique(data$pH_target)),
+              slope = round(coef(gain_lm)[2], 6),
+              intercept = round(coef(gain_lm)[1], 6),
+              Gain = (target * slope) + intercept,
+              rsquared = round(summary(gain_lm)$r.squared, 6)
+            )
+          }, error = function(e) NULL)
+
+}
